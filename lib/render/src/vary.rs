d8be8f1c8f2aafe0e5e6f862d3c02ad445e2a714
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2021-02-28 03:11:30 +0200

    Make Varying an iterator

--- lib/render/src/vary.rs
+++ lib/render/src/vary.rs
@@ -6,7 +6,9 @@ pub struct Varying<T> {
     pub step: T,
 }
 
-impl<T: Linear<f32> + Copy> Varying<T> {
+impl<T> Varying<T>
+    where T: Linear<f32> + Copy
+{
     pub fn from(val: T, step: T) -> Self {
         Self { val, step }
     }
@@ -15,23 +17,30 @@ impl<T: Linear<f32> + Copy> Varying<T> {
         let step = (b.add(a.mul(-1.0))).mul(1.0 / steps);
         Self { val: a, step }
     }
+}
 
-    pub fn step(&mut self) -> T {
+impl<T> Iterator for Varying<T>
+    where T: Linear<f32> + Copy
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
         let res = self.val;
         self.val = self.val.add(self.step);
-        res
+        Some(res)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::vary::Varying;
+
     #[test]
     fn test_varying_f32() {
-        let mut v = Varying::between(0.0, 3.0, 3.0);
-        assert_eq!([0.0, 1.0, 2.0, 3.0], [v.step(), v.step(), v.step(), v.step()]);
+        let v = Varying::between(0.0, 3.0, 3.0);
+        assert_eq!(vec![0.0, 1.0, 2.0, 3.0], v.take(4).collect::<Vec<_>>());
 
-        let mut v = Varying::between(100.0, -50.0, 3.0);
-        assert_eq!([100.0, 50.0, 0.0, -50.0], [v.step(), v.step(), v.step(), v.step()]);
+        let v = Varying::between(100.0, -50.0, 3.0);
+        assert_eq!(vec![100.0, 50.0, 0.0, -50.0], v.take(4).collect::<Vec<_>>());
     }
 }
