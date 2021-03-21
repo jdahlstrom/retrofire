78093995e699f2ff15437e6a5edf50865c433770
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2021-03-21 15:49:49 +0200

    Implement polyline approximation for Bezier curves

--- lib/math/src/spline.rs
+++ lib/math/src/spline.rs
@@ -1,6 +1,7 @@
 use crate::{lerp, Linear};
 use crate::transform::Transform;
 use crate::mat::Mat4;
+use std::fmt::Debug;
 
 pub fn smoothstep(t: f32) -> f32 {
     interpolate(t, 0.0, 1.0, || {
@@ -18,12 +19,10 @@ pub fn interpolate<T, F>(t: f32, min: T, max: T, f: F) -> T
 where
     F: FnOnce() -> T
 {
-    if t < 0.0 {
-        min
-    } else if t < 1.0 {
-        f()
-    } else {
-        max
+    match t {
+        _ if t <= 0.0 => min,
+        _ if t >= 1.0 => max,
+        _ => f()
     }
 }
 
@@ -34,7 +33,7 @@ impl<T: Linear<f32> + Copy> Bezier<T> {
 
     pub fn eval(&self, t: f32) -> T {
         let [p0, p1, p2, p3] = self.0;
-        interpolate(t, p0, p1, || {
+        interpolate(t, p0, p3, || {
             let p01 = lerp(t, p0, p1);
             let p12 = lerp(t, p1, p2);
             let p23 = lerp(t, p2, p3);
@@ -44,14 +43,14 @@ impl<T: Linear<f32> + Copy> Bezier<T> {
 
     pub fn fast_eval(&self, t: f32) -> T {
         let [p0, p1, p2, p3] = self.0;
-        interpolate(t, p0, p1, || {
+        interpolate(t, p0, p3, || {
             let p2_3 = p1.mul(3.0);
             let p3_3 = p2.mul(3.0);
 
             let d0 = p0;
             let d1 = p0.neg().add(p2_3);
             let d2 = p0.add(p2_3).mul(-2.0).add(p3_3);
-            let d3 = p0.add(p2_3).add(p3_3.neg()).add(p3);
+            let d3 = p0.add(p2_3).sub(p3_3).add(p3);
 
             d0.add(d1.add(d2.add(d3.mul(t)).mul(t)).mul(t))
         })
@@ -67,7 +66,7 @@ impl<T: Linear<f32> + Copy> Bezier<T> {
 
         let d0 = p0.neg().add(p2_3);
         let d1 = p0.add(p2_3).mul(-2.0).add(p3_3).mul(2.0);
-        let d2 = p0.add(p2_3).add(p3_3.neg()).add(p3).mul(3.0);
+        let d2 = p0.add(p2_3).sub(p3_3).add(p3).mul(3.0);
 
         d0.add(d1.add(d2.mul(t)).mul(t))
     }
@@ -90,19 +89,63 @@ impl<T: Linear<f32> + Copy> BezierCurve<T> {
         Self(pts.to_vec())
     }
 
+    /// Evaluates this Bezier curve at position `t`.
     pub fn eval(&self, t: f32) -> T {
         interpolate(t, self.0[0], self.0[self.0.len() - 1], || {
-            let segs = ((self.0.len() - 1) / 3) as f32;
-
-            let t = t * segs;
-            let seg = t.floor().min(segs - 1.0);
-            let t = t - seg;
-
-            let idx = 3 * (seg as usize);
-
-            let cps = &self.0[idx..idx + 4];
-            Bezier([cps[0], cps[1], cps[2], cps[3]]).eval(t)
+            let (t, seg) = self.segment(t);
+            Bezier([seg[0], seg[1], seg[2], seg[3]]).eval(t)
         })
+    }
+
+    pub fn tangent(&self, t: f32) -> T {
+        let (t, seg) = self.segment(t);
+        Bezier([seg[0], seg[1], seg[2], seg[3]]).tangent(t)
+    }
+
+    fn segment(&self, t: f32) -> (f32, &[T]) {
+        let segs = ((self.0.len() - 1) / 3) as f32;
+        let seg = (t * segs).floor().min(segs - 1.0);
+        let t2 = t * segs - seg;
+        let idx = 3 * (seg as usize);
+        //eprintln!("t={} -> seg={}, t={}", t, seg, t2);
+        (t2, &self.0[idx..idx + 4])
+    }
+
+    /// Approximates this Bezier curve as a sequence of line segments.
+    ///
+    /// Recursively subdivides the curve into two half-curves, stopping
+    /// once `halt` returns `true`.
+    pub fn approximate(&self, halt: &impl Fn(T, T) -> bool) -> Vec<T> {
+        let mut res = Vec::with_capacity(3 * self.0.len());
+        self.do_approx(0.0, 1.0, 10, halt, &mut res);
+        res.push(self.0[self.0.len()-1]);
+        res
+    }
+
+    fn do_approx(
+        &self,
+        a: f32, b: f32,
+        max_dep: usize,
+        halt: &impl Fn(T, T) -> bool,
+        accum: &mut Vec<T>
+    ) {
+        let mid = 0.5 * (a + b);
+
+        let midp = self.eval(mid);
+
+        let ap = self.eval(a);
+        let bp = self.eval(b);
+
+        let a_to_b = bp.sub(ap);
+        let a_to_mid = midp.sub(ap);
+
+        if max_dep == 0 || halt(a_to_b.mul(0.5), a_to_mid) {
+            //eprintln!("Halting! d={} a={} b={} ab={:?}", max_dep, a, b, a_to_b);
+            accum.push(ap);
+        } else {
+            self.do_approx(a, mid, max_dep - 1, halt, accum);
+            self.do_approx(mid, b, max_dep - 1, halt, accum);
+        }
     }
 }
 
@@ -111,6 +154,7 @@ impl<T: Linear<f32> + Copy> BezierCurve<T> {
 mod tests {
     use super::*;
     use crate::tests::util::*;
+    use crate::vec::dir;
 
     #[test]
     fn bezier_spline_eval_eq_fasteval() {
@@ -137,6 +181,43 @@ mod tests {
         assert_eq!(0.0, b.tangent(1.0));
         assert_eq!(0.0, b.tangent(2.0));
 
+    }
+
+    #[test]
+    fn bezier_curve_approx_1d() {
+        let b = BezierCurve::new(&[2.0, 5.0, -5.0, 10.0]);
+
+        let approx = b.approximate(&|d0: f32, d1: f32| (1.0 - d1/d0).abs() < 0.5);
+
+        for pt in approx.into_iter().step_by(1) {
+            eprintln!("{:>w$}", '*', w = (pt * 10.0) as usize);
+        }
+
+        for i in 0..16 {
+            let pt = b.eval(i as f32 / 15.0);
+            eprintln!("{:>w$}", '*', w = (pt * 10.0) as usize);
+        }
+    }
+
+    #[test]
+    fn bezier_curve_approx_2d() {
+        let b = BezierCurve::new(&[(0.0, 0.0), (0.0, 10.0), (1.0, 10.0), (1.0, 0.0)]);
+
+        let approx = b.approximate(&|(x0, y0), (x1, y1)| {
+            let d0 = dir(x0, y0, 0.0);
+            let d1 = dir(x1, y1, 0.0);
+
+            (1.0 - d0.scalar_project(d1)).abs() < 0.2
+        });
+
+        for pt in approx.into_iter().step_by(1) {
+            eprintln!("{:>w$}", '*', w = (pt.1 * 10.0) as usize);
+        }
+
+        for i in 0..16 {
+            let pt = b.eval(i as f32 / 15.0);
+            eprintln!("{:>w$}", '*', w = (pt.1 * 10.0) as usize);
+        }
     }
 
     #[test]
