9abc7f399ee326fea7d0268c65f54345078a52fc
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2021-10-19 02:58:05 +0300

    Move SdlRunner to a dedicated crate

--- lib/front/src/sdl.rs
+++ lib/front/src/sdl.rs
@@ -1,25 +1,47 @@
-use std::time::Instant;
-
+use render::{Framebuf, Stats};
 use sdl2::{EventPump, Sdl};
 use sdl2::event::Event;
 use sdl2::keyboard::{Keycode, Scancode};
 use sdl2::pixels::Color as SdlColor;
-use sdl2::rect::Rect;
 use sdl2::video::Window;
-
-use render::{Framebuf, Stats};
-use util::Buffer;
-use util::color::{Color, rgb};
-use util::io::save_ppm;
 use std::ops::ControlFlow::{self, *};
+use std::time::Instant;
+use util::Buffer;
+use util::color::rgb;
+use util::io::save_ppm;
 
 pub struct SdlRunner {
     #[allow(unused)]
     sdl: Sdl,
+    opts: Options,
+    event_pump: EventPump,
     window: Window,
     zbuf: Buffer<f32>,
-    event_pump: EventPump,
     start: Instant,
+}
+
+pub struct Options {
+    pub win_w: u32,
+    pub win_h: u32,
+    pub title: String,
+    pub fill_color: Option<SdlColor>,
+    pub fullscreen: bool,
+    pub rel_mouse: bool,
+    pub depth_buf: bool,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            win_w: 720,
+            win_h: 480,
+            title: "=r/e/t/r/o/f/i/r/e=".into(),
+            fill_color: Some(SdlColor::BLACK),
+            fullscreen: false,
+            rel_mouse: false,
+            depth_buf: true
+        }
+    }
 }
 
 pub struct Frame<'a> {
@@ -42,58 +64,60 @@ impl<'a> Frame<'a> {
     }
 }
 
-pub struct ColorBuf<'a>(Buffer<u8, &'a mut [u8]>);
-
-impl<'a> ColorBuf<'a> {
-    pub fn put(&mut self, x: usize, y: usize, c: Color) {
-        let buf = &mut self.0;
-        let idx = 4 * (buf.width() * y + x);
-        let [_, r, g, b] = c.to_argb();
-        buf.data_mut()[idx + 0] = b;
-        buf.data_mut()[idx + 1] = g;
-        buf.data_mut()[idx + 2] = r;
-    }
-    pub fn buffer(&self) -> &Buffer<u8, &'a mut [u8]> {
-        &self.0
-    }
-}
-
 impl SdlRunner {
     pub fn new(win_w: u32, win_h: u32) -> Result<SdlRunner, String> {
-        let sdl = sdl2::init()?;
-        let window = sdl.video()?
-            .window("=r/e/t/r/o/f/i/r/e=", win_w, win_h)
-            .build()
-            .map_err(|e| e.to_string())?;
-        let event_pump = sdl.event_pump()?;
-        sdl.mouse().set_relative_mouse_mode(true);
+        Self::with_options(Options {
+            win_w, win_h,
+            ..Options::default()
+        })
+    }
 
+    pub fn with_options(opts: Options) -> Result<SdlRunner, String> {
+        let sdl = sdl2::init()?;
+        let mut window = sdl.video()?
+            .window(&opts.title, opts.win_w, opts.win_h);
+        if opts.fullscreen {
+            window.fullscreen();
+        }
+        sdl.mouse().set_relative_mouse_mode(opts.rel_mouse);
+
+        let (z_w, z_h) = if opts.depth_buf {
+            (opts.win_w as usize, opts.win_h as usize)
+        } else {
+            (0, 0)
+        };
+
+        let event_pump = sdl.event_pump()?;
         Ok(SdlRunner {
-            sdl,
-            window,
-            event_pump,
-            zbuf: Buffer::new(win_w as usize, win_h as usize, f32::INFINITY),
+            sdl, opts, event_pump,
+            window: window.build().map_err(|e| e.to_string())?,
+            zbuf: Buffer::new(z_w, z_h, f32::INFINITY),
             start: Instant::now(),
         })
     }
 
     pub fn run<F>(&mut self, mut frame_fn: F) -> Result<(), String>
-        where F: FnMut(Frame) -> ControlFlow<Result<(), String>>
+    where
+        F: FnMut(Frame) -> ControlFlow<Result<(), String>>
     {
         let mut clock = Instant::now();
         loop {
-            let events = self.event_pump.poll_iter()
+            let ep = &mut self.event_pump;
+
+            let events = ep.poll_iter()
                 .collect::<Vec<_>>();
-            let pressed_keys = self.event_pump.keyboard_state()
+            let pressed_keys = ep.keyboard_state()
                 .pressed_scancodes().collect();
 
             if events.iter().any(is_quit) {
                 return Ok(());
             }
 
-            let mut surf = self.window.surface(&self.event_pump)?;
-            let rect = Rect::new(0, 0, surf.width(), surf.height());
-            surf.fill_rect(rect, SdlColor::GREY)?;
+            let mut surf = self.window.surface(ep)?;
+
+            if let Some(c) = self.opts.fill_color {
+                surf.fill_rect(None, c)?;
+            }
 
             let zbuf = &mut self.zbuf;
             zbuf.fill(f32::INFINITY);
@@ -121,7 +145,6 @@ impl SdlRunner {
         }
     }
 
-    #[allow(unused)]
     pub fn pause(&mut self) {
         self.event_pump.wait_iter().find(is_quit);
     }
@@ -143,6 +166,7 @@ impl SdlRunner {
 }
 
 fn is_quit(e: &Event) -> bool {
-    matches!(e, Event::Quit { .. }
-           | Event::KeyDown { keycode: Some(Keycode::Escape), .. })
+    matches!(e,
+        | Event::Quit { .. }
+        | Event::KeyDown { keycode: Some(Keycode::Escape), .. })
 }
