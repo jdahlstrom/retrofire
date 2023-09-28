313ea245980edb5b52f01ced6691df579024bfe2
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2023-09-28 23:13:20 +0300

    Improve texture doc comments

--- core/src/render/tex.rs
+++ core/src/render/tex.rs
@@ -1,26 +1,47 @@
+//! Textures and texture samplers.
+
 use crate::math::vec::{Real, Vec2, Vector};
 use crate::util::buf::{AsSlice2, Buf2, Slice2};
 
-// Basis of the texture space
+/// Basis of the texture space.
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
 pub struct Tex;
 
+/// Texture coordinate vector. Texture coordinates can be either absolute,
+/// in range (0, 0)..(w, h) for some texture with dimensions w and h, or
+/// relative, in range (0, 0)..(1, 1), in which case they are independent
+/// of the actual dimensions of the texture.
 pub type TexCoord = Vec2<Real<2, Tex>>;
 
 impl TexCoord {
+    /// Returns the u (horizontal) component of `self`.
     pub fn u(&self) -> f32 {
         self.0[0]
     }
+    /// Returns the v (vertical) component of `self`.
     pub fn v(&self) -> f32 {
         self.0[1]
     }
 }
 
+/// Returns a new texture coordinate with components `u` and `v`.
 #[inline]
 pub const fn uv(u: f32, v: f32) -> TexCoord {
     Vector::new([u, v])
 }
 
+/// A texture type. Can contain either owned or borrowed pixel data.
+///
+/// Textures are used to render *texture mapped* geometry, by interpolating
+/// texture coordinates across polygon faces. To read, or *sample*, from a
+/// `Texture`, use one of the `Sampler*` types defined in this module.
+///
+/// Multiple textures can be packed into a single larger memory buffer, often
+/// called a "texture atlas" or "sprite sheet". Each texture borrows a region
+/// of the larger buffer.
+///
+/// * TODO Mipmapping
+/// * TODO Bilinear filtering sampler
 #[derive(Copy, Clone)]
 pub struct Texture<D> {
     w: f32,
@@ -29,10 +50,12 @@ pub struct Texture<D> {
 }
 
 impl<D> Texture<D> {
+    /// Returns the width of `Self` as `f32`.
     #[inline]
     pub fn width(&self) -> f32 {
         self.w
     }
+    /// Returns the height of `Self` as `f32`.
     #[inline]
     pub fn height(&self) -> f32 {
         self.h
@@ -40,6 +63,7 @@ impl<D> Texture<D> {
 }
 
 impl<C> From<Buf2<C>> for Texture<Buf2<C>> {
+    /// Creates a new texture from owned pixel data.
     fn from(data: Buf2<C>) -> Self {
         Self {
             w: data.width() as f32,
@@ -50,6 +74,7 @@ impl<C> From<Buf2<C>> for Texture<Buf2<C>> {
 }
 
 impl<'a, C> From<Slice2<'a, C>> for Texture<Slice2<'a, C>> {
+    /// Creates a new texture from borrowed pixel data.
     fn from(data: Slice2<'a, C>) -> Self {
         Self {
             w: data.width() as f32,
@@ -70,6 +95,9 @@ pub struct SamplerRepeatPot {
 
 #[cfg(feature = "std")]
 impl SamplerRepeatPot {
+    /// Creates a new `SamplerRepeatPot` based on the dimensions of `tex`.
+    /// # Panics
+    /// If the width or height of `tex` is not a power of two.
     pub fn new<C>(tex: &Texture<impl AsSlice2<C>>) -> Self {
         let w = tex.width() as u32;
         let h = tex.height() as u32;
@@ -81,6 +109,10 @@ impl SamplerRepeatPot {
         }
     }
 
+    /// Returns the color in `tex` at `tc` in relative coordinates, such that
+    /// coordinates outside `0.0..1.0` are wrapped to the valid range.
+    ///
+    /// Uses nearest neighbor sampling.
     pub fn sample<C: Copy>(
         &self,
         tex: &Texture<impl AsSlice2<C>>,
@@ -90,6 +122,11 @@ impl SamplerRepeatPot {
         self.sample_abs(tex, scaled_uv)
     }
 
+    /// Returns the color in `tex` at `tc` in absolute coordinates, such that
+    /// coordinates outside `0.0..tex.width()` and `0.0..tex.height()` are
+    /// wrapped to the valid range.
+    ///
+    /// Uses nearest neighbor sampling.
     pub fn sample_abs<C: Copy>(
         &self,
         tex: &Texture<impl AsSlice2<C>>,
@@ -98,7 +135,6 @@ impl SamplerRepeatPot {
         let u = (tc.u()).floor() as i32 & self.w_mask;
         let v = (tc.v()).floor() as i32 & self.h_mask;
 
-        // TODO enforce invariants and use get_unchecked
         tex.data.as_slice2()[[u, v]]
     }
 }
@@ -110,6 +146,10 @@ pub struct SamplerClamp;
 
 #[cfg(feature = "std")]
 impl SamplerClamp {
+    /// Returns the color in `tex` at `tc` such that coordinates outside
+    /// the range `0.0..1.0` are clamped to the range endpoints.
+    ///
+    /// Uses nearest neighbor sampling.
     pub fn sample<C: Copy>(
         &self,
         tex: &Texture<impl AsSlice2<C>>,
@@ -118,6 +158,11 @@ impl SamplerClamp {
         self.sample_abs(tex, uv(tc.u() * tex.w, tc.v() * tex.h))
     }
 
+    /// Returns the color in `tex` at `tc` in absolute coordinates, such that
+    /// coordinates outside `0.0..tex.width()` and `0.0..tex.height()` are
+    /// clamped to the range endpoints.
+    ///
+    /// Uses nearest neighbor sampling.
     pub fn sample_abs<C: Copy>(
         &self,
         tex: &Texture<impl AsSlice2<C>>,
@@ -132,13 +177,21 @@ impl SamplerClamp {
 /// A texture sampler that assumes all texture coordinates are within bounds.
 ///
 /// Out-of-bounds coordinates may cause graphical glitches or runtime panics
-/// but no undefined behavior. In particular, if the texture data is a slice
+/// but not undefined behavior. In particular, if the texture data is a slice
 /// of a larger buffer, `SamplerOnce` may read out of bounds of the slice but
 /// not the backing buffer.
 #[derive(Copy, Clone, Debug)]
 pub struct SamplerOnce;
 
 impl SamplerOnce {
+    /// Returns the color in `tex` at `tc` such that both coordinates are
+    /// assumed to be in the range `0.0..1.0`.
+    ///
+    /// Uses nearest neighbor sampling. Passing out-of-range coordinates
+    /// to this function is sound (not UB) but is not otherwise specified.
+    ///
+    /// # Panics
+    /// May panic if `tc` is not in the valid range.
     pub fn sample<C: Copy>(
         &self,
         tex: &Texture<impl AsSlice2<C>>,
@@ -147,6 +200,15 @@ impl SamplerOnce {
         let scaled_uv = uv(tex.width() * tc.u(), tex.height() * tc.v());
         self.sample_abs(tex, scaled_uv)
     }
+    /// Returns the color in `tex` at `tc` such that the coordinates are
+    /// assumed to be in the ranges `0.0..tex.width()` and `0.0..tex.height()`
+    /// respectively.
+    ///
+    /// Uses nearest neighbor sampling. Passing out-of-range coordinates
+    /// to this function is sound (not UB) but is not otherwise specified.
+    ///
+    /// # Panics
+    /// May panic if `tc` is not in the valid range.
     pub fn sample_abs<C: Copy>(
         &self,
         tex: &Texture<impl AsSlice2<C>>,
