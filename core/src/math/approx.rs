cbca746e5ff15fd1112fb4aed0d1509a15515e23
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2024-01-15 18:23:55 +0200

    Add floating-point function adapter
    The functions exposed in core::math::float::f32 automatically delegate to either
    std, libm, or micromath, depending on what features are enabled. This lets code
    be agnostic of which implementation of fp functions is actually used.
    
    There is also a fallback implementing a few common and simple functions even
    when none of the fp features is enabled.

--- core/src/math/approx.rs
+++ core/src/math/approx.rs
@@ -34,10 +34,9 @@ pub trait ApproxEq<Other: ?Sized = Self, Epsilon = Self> {
 
 impl ApproxEq for f32 {
     fn approx_eq_eps(&self, other: &Self, rel_eps: &Self) -> bool {
-        // TODO Always provide abs
-        let diff = self.max(*other) - self.min(*other);
-        let abs_self = if *self >= 0.0 { *self } else { -self };
-        diff <= *rel_eps * abs_self.max(1.0)
+        use super::float::f32;
+        let diff = f32::abs(self - other);
+        diff <= *rel_eps * f32::abs(*self).max(1.0)
     }
     fn relative_epsilon() -> Self {
         Self::EPSILON
