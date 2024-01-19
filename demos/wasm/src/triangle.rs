288d637f284a22874fbafb9fe48a5822c53884a1
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2024-01-19 04:05:27 +0200

    Add a WebAssembly demo

---
+++ demos/wasm/src/triangle.rs
@@ -0,0 +1,59 @@
+use core::ops::ControlFlow::*;
+
+use wasm_bindgen::prelude::*;
+
+use re::geom::{vertex, Tri, Vertex};
+use re::math::{
+    color::{rgba, Color4f},
+    mat::{perspective, rotate_z, translate, viewport},
+    rads, vec2, vec3,
+};
+use re::render::{raster::Frag, render, shader::Shader, ModelToView};
+
+use re_front::wasm::Window;
+
+// Entry point from JS
+#[wasm_bindgen(start)]
+pub fn start() {
+    const W: u32 = 640;
+    const H: u32 = 480;
+
+    console_error_panic_hook::set_once();
+
+    let mut win = Window::new(W, H).expect("could not create window");
+    win.ctx.color_clear = Some(rgba(0, 0, 0, 0x80));
+
+    let vs = [
+        vertex(vec3(-2.0, 1.0, 0.0).to(), rgba(1.0, 0.2, 0.1, 0.9)),
+        vertex(vec3(2.0, 2.0, 0.0).to(), rgba(0.2, 0.9, 0.1, 0.8)),
+        vertex(vec3(0.0, -2.0, 0.0).to(), rgba(0.3, 0.4, 1.0, 1.0)),
+    ];
+
+    let proj = perspective(1.0, 4.0 / 3.0, 0.1..1000.0);
+    let vp = viewport(vec2(8, 8)..vec2(W - 8, H - 8));
+
+    win.run(move |frame| {
+        let t = frame.t.as_secs_f32();
+
+        let mv = rotate_z(rads(t))
+            .then(&translate(vec3(0.0, 0.0, 3.0 + 2.0 * t.sin())))
+            .to::<ModelToView>();
+        let mvp = mv.then(&proj);
+
+        let sh = Shader::new(
+            |v: Vertex<_, Color4f>, _| vertex(mvp.apply(&v.pos), v.attrib),
+            |f: Frag<Color4f>| f.var.to_color4(),
+        );
+
+        render(
+            &[Tri([0, 1, 2])],
+            vs,
+            &sh,
+            (),
+            vp,
+            &mut frame.buf,
+            &mut frame.ctx,
+        );
+        Continue(())
+    });
+}
