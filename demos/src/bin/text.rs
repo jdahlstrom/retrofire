ce26b0d8eb1b13b9ddc544080bf17ad7c2f8e82c
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2024-06-20 01:20:05 +0300

    WIP text demo

--- demos/src/bin/text.rs
+++ demos/src/bin/text.rs
@@ -1,12 +1,12 @@
 use std::ops::ControlFlow::Continue;
 
-use re::math::mat::scale;
 use re::prelude::*;
 
 use re::math::space::Real;
 use re::render::tex::{uv, Atlas, SamplerClamp, TexCoord};
-use re::render::{render, Model, ModelToProj, ModelToView};
+use re::render::{render, Model, ModelToProj};
 use re::util::pnm::read_pnm;
+
 use re_front::minifb::Window;
 use re_front::Frame;
 
@@ -18,11 +18,12 @@ fn main() {
         vertex(vec3(1.0, 1.0, 0.0).to(), uv(1.0, 1.0)),
     ];
 
-    let (cw, ch) = (12, 20);
     let font = *include_bytes!("../../../assets/font_12x20.pbm");
-    let font = Atlas::new(cw, ch, read_pnm(font).unwrap());
+    let font = read_pnm(font).unwrap();
+    let (cw, ch) = dbg!((font.width() / 16, font.height() / 16));
+    let font = Atlas::new(cw, ch, font);
 
-    let text = b"Hello, World!";
+    let text = b"Hello, World! \x01";
     let mut buf = Buf2::new(cw * text.len() as u32, ch);
     let mut x = 0;
     for &c in text {
@@ -37,7 +38,7 @@ fn main() {
         .size(640, 480)
         .build();
 
-    //win.ctx.color_clear = Some(rgba(0xff, 0xff, 0xff, 0xff));
+    win.ctx.face_cull = None;
 
     let shader = Shader::new(
         |v: Vertex<_, _>, mvp: &Mat4x4<ModelToProj>| {
@@ -46,20 +47,21 @@ fn main() {
         |frag: Frag<TexCoord>| SamplerClamp.sample(&tex, frag.var).to_rgba(),
     );
 
-    let modelview = translate(vec3(0.0, 0.0, 2.0));
-    let project = perspective(1.0, 4.0 / 3.0, 0.1..1000.0);
+    let mvp = translate(vec3(0.0, 0.0, 2.0))
+        .to()
+        .then(&perspective(1.0, 4.0 / 3.0, 0.1..1000.0));
+
     let viewport = viewport(vec2(10, 10)..vec2(630, 470));
 
     win.run(|frame: &mut Frame<_>| {
         let secs = frame.t.as_secs_f32();
 
-        let mv: Mat4x4<ModelToView> = modelview
-            .compose(&rotate_z(rads(secs)))
-            .compose(&rotate_y(rads((secs * 1.3).sin())))
+        let mvp: Mat4x4<ModelToProj> = mvp
+            .compose(&rotate_z(rads((secs * 1.13).sin())))
+            .compose(&rotate_y(rads(secs * 0.59)))
             .compose(&translate(vec3(0.0, 0.0, secs.sin())))
             .compose(&scale(vec3(1.0, 0.1, 1.0)))
             .to();
-        let mvp = mv.then(&project);
 
         render(
             [Tri([0, 1, 2]), Tri([3, 2, 1])],
