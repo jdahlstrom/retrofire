d349d3b6588e9eb2ef4d159c9fca1a9e28c5f2f9
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2024-07-21 14:36:12 +0300

    Improve frontend error handling
    Change panics to return Results instead.

--- front/src/sdl2.rs
+++ front/src/sdl2.rs
@@ -10,7 +10,6 @@ use sdl2::{
 };
 
 use retrofire_core::math::Vary;
-
 use retrofire_core::render::{
     ctx::Context, raster::Scanline, shader::FragmentShader, stats::Throughput,
     target::Target,
@@ -22,9 +21,9 @@ use crate::{dims, Frame};
 /// A lightweight wrapper of an `SDL2` window.
 pub struct Window {
     /// The SDL canvas.
-    pub cvs: WindowCanvas,
+    pub canvas: WindowCanvas,
     /// The SDL event pump.
-    pub ep: EventPump,
+    pub ev_pump: EventPump,
     /// The width and height of the window.
     pub dims: (u32, u32),
     /// Rendering context defaults.
@@ -73,34 +72,32 @@ impl<'t> Builder<'t> {
     }
 
     /// Creates the window.
-    pub fn build(self) -> Window {
+    pub fn build(self) -> Result<Window, String> {
         let Self { dims, title, vsync } = self;
-        let sdl = sdl2::init().expect("could not initialize SDL");
 
-        let mut cvs = sdl
-            .video()
-            .expect("could not get SDL video subsystem")
+        let sdl = sdl2::init()?;
+
+        let mut canvas = sdl
+            .video()?
             .window(title, dims.0, dims.1)
             .build()
-            .expect("could not open SDL window")
+            .map_err(|e| e.to_string())?
             .into_canvas();
 
         if vsync {
-            cvs = cvs.present_vsync();
+            canvas = canvas.present_vsync();
         }
 
-        let cvs = cvs
+        let canvas = canvas
             .accelerated()
             .build()
-            .expect("could not create SDL canvas");
+            .map_err(|e| e.to_string())?;
 
-        let ep = sdl
-            .event_pump()
-            .expect("could not get SDL event pump");
+        let ev_pump = sdl.event_pump()?;
 
         let ctx = Context::default();
 
-        Window { cvs, ep, dims, ctx }
+        Ok(Window { canvas, ev_pump, dims, ctx })
     }
 }
 
@@ -110,11 +107,10 @@ impl Window {
         Builder::default()
     }
 
-    pub fn present(&mut self, tex: &Texture) {
-        self.cvs
-            .copy(&tex, None, None)
-            .expect("could not copy frame to canvas");
-        self.cvs.present();
+    pub fn present(&mut self, tex: &Texture) -> Result<(), String> {
+        self.canvas.copy(&tex, None, None)?;
+        self.canvas.present();
+        Ok(())
     }
 
     /// Runs the main loop of the program, invoking the callback on each
@@ -124,16 +120,16 @@ impl Window {
     /// * the user closes the window via the GUI (e.g. title bar close button);
     /// * the Esc key is pressed; or
     /// * the callback returns `ControlFlow::Break`.
-    pub fn run<F>(&mut self, mut frame_fn: F)
+    pub fn run<F>(&mut self, mut frame_fn: F) -> Result<(), String>
     where
         F: FnMut(&mut Frame<Self, Framebuf>) -> ControlFlow<()>,
     {
         let (w, h) = self.dims;
 
-        let tc = self.cvs.texture_creator();
+        let tc = self.canvas.texture_creator();
         let mut tex = tc
             .create_texture_streaming(None, w, h)
-            .expect("could not create output texture");
+            .map_err(|e| e.to_string())?;
 
         let mut zbuf = Buf2::new(self.dims);
         let mut ctx = self.ctx.clone();
@@ -141,7 +137,7 @@ impl Window {
         let start = Instant::now();
         let mut last = Instant::now();
         'main: loop {
-            for e in self.ep.poll_iter() {
+            for e in self.ev_pump.poll_iter() {
                 match e {
                     Event::Quit { .. }
                     | Event::KeyDown {
@@ -151,36 +147,34 @@ impl Window {
                 }
             }
 
-            let cf = tex
-                .with_lock(None, |cbuf, pitch| {
-                    if let Some(c) = ctx.depth_clear {
-                        // Z-buffer stores reciprocals
-                        zbuf.fill(c.recip());
-                    }
-                    if let Some(c) = ctx.color_clear {
-                        let [r, g, b, a] = c.0;
-                        cbuf.chunks_exact_mut(4).for_each(|ch| {
-                            ch.copy_from_slice(&[b, g, r, a]);
-                        });
-                    }
+            let cf = tex.with_lock(None, |cbuf, pitch| {
+                if let Some(c) = ctx.depth_clear {
+                    // Z-buffer stores reciprocals
+                    zbuf.fill(c.recip());
+                }
+                if let Some(c) = ctx.color_clear {
+                    let [r, g, b, a] = c.0;
+                    cbuf.chunks_exact_mut(4).for_each(|ch| {
+                        ch.copy_from_slice(&[b, g, r, a]);
+                    });
+                }
 
-                    let color_buf =
-                        MutSlice2::new((4 * w, h), pitch as u32, cbuf);
-                    let depth_buf = zbuf.as_mut_slice2();
+                let buf = Framebuf {
+                    color_buf: MutSlice2::new((4 * w, h), pitch as u32, cbuf),
+                    depth_buf: zbuf.as_mut_slice2(),
+                };
 
-                    let frame = &mut Frame {
-                        t: start.elapsed(),
-                        dt: replace(&mut last, Instant::now()).elapsed(),
-                        buf: Framebuf { color_buf, depth_buf },
-                        win: self,
-                        ctx: &mut ctx,
-                    };
+                let frame = &mut Frame {
+                    t: start.elapsed(),
+                    dt: replace(&mut last, Instant::now()).elapsed(),
+                    buf,
+                    win: self,
+                    ctx: &mut ctx,
+                };
+                frame_fn(frame)
+            })?;
 
-                    frame_fn(frame)
-                })
-                .expect("could not render to texture");
-
-            self.present(&tex);
+            self.present(&tex)?;
             ctx.stats.borrow_mut().frames += 1.0;
 
             if cf.is_break() {
@@ -188,6 +182,7 @@ impl Window {
             }
         }
         println!("{}", ctx.stats.borrow());
+        Ok(())
     }
 }
 
