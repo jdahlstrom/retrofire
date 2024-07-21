35710a27278d39bb8db113d929ec531294748106
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2024-07-21 18:35:55 +0300

    Move Normal type aliases to core, add to prelude

--- core/src/geom.rs
+++ core/src/geom.rs
@@ -1,5 +1,7 @@
 //! Basic geometric primitives.
 
+use crate::math::vec::{Vec2, Vec3};
+
 pub use mesh::Mesh;
 
 pub mod mesh;
@@ -20,6 +22,11 @@ pub struct Tri<V>(pub [V; 3]);
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 #[repr(transparent)]
 pub struct Plane<V>(pub(crate) V);
+
+/// A surface normal.
+// TODO Use distinct type rather than alias
+pub type Normal3 = Vec3;
+pub type Normal2 = Vec2;
 
 pub const fn vertex<P, A>(pos: P, attrib: A) -> Vertex<P, A> {
     Vertex { pos, attrib }
