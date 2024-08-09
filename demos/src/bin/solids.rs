6e46c09271b9c49195bee115fab36a510dd8917b
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2024-08-09 23:50:16 +0300

    Clean up solids demo a lot
    Add type aliases and move object creation into functions

--- demos/src/bin/solids.rs
+++ demos/src/bin/solids.rs
@@ -4,11 +4,15 @@ use minifb::{Key, KeyRepeat};
 
 use re::prelude::*;
 
-use re::math::{color::gray, mat::RealToReal, spline::smootherstep};
-use re::render::{cam::Camera, ModelToProj, ModelToWorld};
-use re_front::minifb::Window;
+use re::math::{
+    color::gray, mat::RealToReal, spline::smootherstep, vec::ProjVec4,
+};
+use re::render::batch::Batch;
+use re::render::{cam::Camera, Model, ModelToProj, ModelToWorld};
+use re_front::{minifb::Window, Frame};
 use re_geom::{io::parse_obj, solids::*};
 
+// Carousel animation for switching between objects.
 #[derive(Default)]
 struct Carousel {
     idx: usize,
@@ -55,20 +59,120 @@ fn main() {
 
     win.ctx.color_clear = Some(gray(32).to_rgba());
 
-    let shader = Shader::new(
-        |v: Vertex<_, Normal3>,
-         (mvp, spin): (&Mat4x4<ModelToProj>, &Mat4x4<RealToReal<3>>)| {
-            let n = spin.apply(&v.attrib.to());
-            let diffuse = (n.z() + 0.2).max(0.2) * 0.8;
+    let cam = Camera::new(W, H)
+        .mode(scale(vec3(1.0, -1.0, -1.0)).to())
+        .perspective(1.5, 0.1..1000.0)
+        .viewport(vec2(10, 10)..vec2(W - 10, H - 10));
 
-            let [x, y, z] = (0.45 * (v.attrib + splat(1.1))).0;
+    type VertexIn = Vertex<Vec3<Model>, Normal3>;
+    type VertexOut = Vertex<ProjVec4, Color3f>;
+    type Uniform<'a> = (&'a Mat4x4<ModelToProj>, &'a Mat4x4<RealToReal<3>>);
 
-            vertex(mvp.apply(&v.pos), rgb(x, y, z).mul(diffuse))
-        },
-        |frag: Frag<Color3f>| frag.var.to_color4(),
-    );
+    fn vtx_shader(v: VertexIn, (mvp, spin): Uniform) -> VertexOut {
+        // Transform vertex normal
+        let norm = spin.apply(&v.attrib.to());
+        // Calculate diffuse shading
+        let diffuse = (norm.z() + 0.2).max(0.2) * 0.8;
+        // Visualize normal by mapping to RGB values
+        let [r, g, b] = (0.45 * (v.attrib + splat(1.1))).0;
+        let col = rgb(r, g, b).mul(diffuse);
+        vertex(mvp.apply(&v.pos), col)
+    }
 
-    let teapot = parse_obj(*include_bytes!("../../assets/teapot.obj"))
+    fn frag_shader(f: Frag<Color3f>) -> Color4 {
+        f.var.to_color4()
+    }
+
+    let shader = Shader::new(vtx_shader, frag_shader);
+
+    let objects = objects(8);
+
+    let translate = translate(vec3(0.0, 0.0, -4.0));
+    let mut carousel = Carousel::default();
+
+    win.run(|frame| {
+        let Frame { t, dt, win, .. } = frame;
+
+        // Press Space to trigger carousel animation
+        if win.imp.is_key_pressed(Key::Space, KeyRepeat::No) {
+            carousel.start();
+        }
+
+        let theta = rads(t.as_secs_f32());
+        let spin = rotate_x(theta * 0.47).then(&rotate_y(theta * 0.61));
+        let carouse = carousel.update(dt.as_secs_f32());
+
+        // Compose transform stack
+        let model_view_project: Mat4x4<ModelToProj> = spin
+            .then(&translate)
+            .then(&carouse)
+            .to::<ModelToWorld>()
+            .then(&cam.world_to_project());
+
+        Batch::new()
+            .mesh(&objects[carousel.idx % objects.len()])
+            .uniform((&model_view_project, &spin))
+            .shader(shader)
+            .viewport(cam.viewport)
+            .target(&mut frame.buf)
+            .context(&*frame.ctx)
+            .render();
+
+        Continue(())
+    });
+}
+
+// Creates the 13 objects exhibited.
+#[rustfmt::skip]
+fn objects(res: u32) -> [Mesh<Normal3>; 13] {
+    let segments = res;
+    let sectors = 2 * res;
+
+    let cap_segments = res;
+
+    let major_sectors = 3 * res;
+    let minor_sectors = 2 * res;
+    [
+        // The five Platonic solids
+        Tetrahedron.build(),
+        Box::cube(1.25).build(),
+        Octahedron.build(),
+        Dodecahedron.build(),
+        Icosahedron.build(),
+
+        // Surfaces of revolution
+        lathe(sectors),
+        Sphere { sectors, segments, radius: 1.0, }.build(),
+        Cylinder { sectors, radius: 0.8, capped: true }.build(),
+        Cone { sectors, base_radius: 1.1, apex_radius: 0.3, capped: true, }.build(),
+        Capsule { sectors, cap_segments, radius: 0.5, }.build(),
+        Torus { major_radius: 0.9, minor_radius: 0.3, major_sectors, minor_sectors, }.build(),
+
+        // Traditional demo models
+        teapot(),
+        bunny(),
+    ]
+}
+
+// Creates a Lathe mesh.
+fn lathe(secs: u32) -> Mesh<Normal3> {
+    Lathe::new(
+        vec![
+            vertex(vec2(0.75, -0.5), vec2(1.0, 1.0)),
+            vertex(vec2(0.55, -0.25), vec2(1.0, 0.5)),
+            vertex(vec2(0.5, 0.0), vec2(1.0, 0.0)),
+            vertex(vec2(0.55, 0.25), vec2(1.0, -0.5)),
+            vertex(vec2(0.75, 0.5), vec2(1.0, 1.0)),
+        ],
+        secs,
+    )
+    .capped(true)
+    .build()
+}
+
+// Loads the Utah teapot model.
+fn teapot() -> Mesh<Normal3> {
+    parse_obj(*include_bytes!("../../assets/teapot.obj"))
         .unwrap()
         .transform(
             &scale(splat(0.4))
@@ -76,9 +180,12 @@ fn main() {
                 .to(),
         )
         .with_vertex_normals()
-        .build();
+        .build()
+}
 
-    let bunny = parse_obj(*include_bytes!("../../assets/bunny.obj"))
+// Loads the Stanford bunny model.
+fn bunny() -> Mesh<Normal3> {
+    parse_obj(*include_bytes!("../../assets/bunny.obj"))
         .unwrap()
         .transform(
             &scale(splat(0.15))
@@ -86,100 +193,5 @@ fn main() {
                 .to(),
         )
         .with_vertex_normals()
-        .build();
-
-    let objs = [
-        // The five Platonic solids
-        Tetrahedron.build(),
-        Box::cube(1.25).build(),
-        Octahedron.build(),
-        Dodecahedron.build(),
-        Icosahedron.build(),
-        // Surfaces of revolution
-        Lathe::new(
-            vec![
-                vertex(vec2(0.75, -0.5), vec2(1.0, 1.0)),
-                vertex(vec2(0.55, -0.25), vec2(1.0, 0.5)),
-                vertex(vec2(0.5, 0.0), vec2(1.0, 0.0)),
-                vertex(vec2(0.55, 0.25), vec2(1.0, -0.5)),
-                vertex(vec2(0.75, 0.5), vec2(1.0, 1.0)),
-            ],
-            13,
-        )
-        .capped(true)
-        .build(),
-        Sphere {
-            sectors: 12,
-            segments: 6,
-            radius: 1.0,
-        }
-        .build(),
-        Cylinder {
-            sectors: 12,
-            capped: true,
-            radius: 0.8,
-        }
-        .build(),
-        Cone {
-            sectors: 12,
-            capped: true,
-            base_radius: 1.1,
-            apex_radius: 0.1,
-        }
-        .build(),
-        Capsule {
-            sectors: 10,
-            cap_segments: 5,
-            radius: 0.5,
-        }
-        .build(),
-        Torus {
-            major_radius: 0.9,
-            minor_radius: 0.3,
-            major_sectors: 16,
-            minor_sectors: 8,
-        }
-        .build(),
-        teapot,
-        bunny,
-    ];
-
-    let cam = Camera::new(W, H)
-        .mode(scale(vec3(1.0, -1.0, -1.0)).to())
-        .perspective(1.5, 0.1..1000.0)
-        .viewport(vec2(10, 10)..vec2(W - 10, H - 10));
-
-    let translate = translate(vec3(0.0, 0.0, -4.0));
-
-    let mut carousel = Carousel::default();
-    win.run(|frame| {
-        let secs = frame.t.as_secs_f32();
-
-        let spin = rotate_x(rads(secs * 0.47)) //
-            .then(&rotate_y(rads(secs * 0.61)));
-        let carouse = carousel.update(frame.dt.as_secs_f32());
-
-        let model_to_world: Mat4x4<ModelToWorld> =
-            spin.then(&translate).then(&carouse).to();
-
-        if frame
-            .win
-            .imp
-            .is_key_pressed(Key::Space, KeyRepeat::No)
-        {
-            carousel.start();
-        }
-
-        let obj = &objs[carousel.idx % objs.len()];
-        cam.render(
-            &obj.faces,
-            &obj.verts,
-            &model_to_world,
-            &shader,
-            &spin,
-            &mut frame.buf,
-            &frame.ctx,
-        );
-        Continue(())
-    });
+        .build()
 }
