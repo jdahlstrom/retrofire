b2eaf3d11b28b5d1637afeaf19395023de3f9e2f
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2024-12-04 20:01:05 +0200

    Add alias, methods to Point

--- core/src/math/point.rs
+++ core/src/math/point.rs
@@ -19,6 +19,9 @@ pub type Point2<Basis = ()> = Point<[f32; 2], Real<2, Basis>>;
 /// A 3-point with `f32` components.
 pub type Point3<Basis = ()> = Point<[f32; 3], Real<3, Basis>>;
 
+/// A 2-point with `u32` components.
+pub type Point2u<Basis = ()> = Point<[u32; 2], Real<2, Basis>>;
+
 /// Returns a real 2-point with `x` and `y` components.
 pub const fn pt2<Sc, B>(x: Sc, y: Sc) -> Point<[Sc; 2], Real<2, B>> {
     Point([x, y], Pd)
@@ -33,6 +36,21 @@ impl<R, Sp> Point<R, Sp> {
     pub fn new(repr: R) -> Self {
         Self(repr, Pd)
     }
+
+    #[inline]
+    pub fn to<T>(self) -> Point<R, T> {
+        Point(self.0, Pd)
+    }
+}
+
+impl<Sc, Sp, const N: usize> Point<[Sc; N], Sp> {
+    /// Returns a vector of the same dimension as `self` by applying `f`
+    /// component-wise.
+    #[inline]
+    #[must_use]
+    pub fn map<T>(self, f: impl FnMut(Sc) -> T) -> Point<[T; N], Sp> {
+        self.0.map(f).into()
+    }
 }
 
 impl<const N: usize, B> Point<[f32; N], Real<N, B>> {
@@ -44,6 +62,23 @@ impl<const N: usize, B> Point<[f32; N], Real<N, B>> {
     #[inline]
     pub fn distance_sqr(&self, other: &Self) -> f32 {
         Affine::sub(self, other).len_sqr()
+    }
+
+    /// Returns `self` clamped component-wise to the given range.
+    ///
+    /// In other words, for each component `self[i]`, the result `r` has
+    /// `r[i]` equal to `self[i].clamp(min[i], max[i])`.
+    ///
+    /// # Examples
+    /// ```
+    /// # use retrofire_core::math::point::{pt3, Point3};
+    /// let pt = pt3::<f32, ()>(0.5, 1.5, -2.0);
+    /// // Clamp to the unit cube
+    /// let clamped = pt.clamp(&pt3(0.0, 0.0, 0.0), &pt3(1.0, 1.0, 1.0));
+    /// assert_eq!(clamped, pt3(0.5, 1.0, 0.0));
+    #[must_use]
+    pub fn clamp(&self, min: &Self, max: &Self) -> Self {
+        array::from_fn(|i| self.0[i].clamp(min.0[i], max.0[i])).into()
     }
 }
 
