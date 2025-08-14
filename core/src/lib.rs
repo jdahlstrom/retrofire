ca65b9aa54254cb36cf22fb8a9a3e8789bce66fc
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2025-08-14 20:32:12 +0300

    Add full example to lib.rs doc comment
    Slight syntax cleanups

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -34,6 +34,47 @@
 //!   via the [micromath](https://crates.io/crates/micromath) crate.
 //!
 //! All features are disabled by default.
+//!
+//! # Example
+//!
+//! ```
+//! use retrofire_core::{prelude::*, util::*};
+//!
+//! let verts = [
+//!     vertex(pt3(-0.8, 1.0, 0.0), rgb(1.0, 0.0, 0.0)),
+//!     vertex(pt3(0.8, 1.0, 0.0), rgb(0.0, 0.8, 0.0)),
+//!     vertex(pt3(0.0, -1.5, 0.0), rgb(0.4, 0.4, 1.0)),
+//! ];
+//!
+//! let shader = shader::new(
+//!     |v: Vertex3<_>, mvp: &Mat4x4<ModelToProj>| {
+//!         vertex(mvp.apply(&v.pos), v.attrib)
+//!     },
+//!     |frag: Frag<Color3f>| frag.var.to_color4(),
+//! );
+//!
+//! let dims @ (w, h) = (640, 480);
+//! let modelview = translate3(0.0, 0.0, 2.0).to();
+//! let project = perspective(1.0, w as f32 / h as f32, 0.1..1000.0);
+//! let viewport = viewport(pt2(0, 0)..pt2(w, h));
+//!
+//! let mut fb = Colorbuf {
+//!     buf: Buf2::new(dims),
+//!     fmt: pixfmt::Xrgb8888,
+//! };
+//!
+//! render(
+//!     [Tri([0, 1, 2])],
+//!     verts,
+//!     &shader,
+//!     &modelview.then(&project),
+//!     viewport,
+//!     &mut fb,
+//!     &Context::default(),
+//! );
+//!
+//! assert_eq!(fb.buf[[w/2, h/2]], 0x00_74_66_65);
+//! ```
 
 #![no_std]
 
