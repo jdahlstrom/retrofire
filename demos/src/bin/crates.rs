bd1f425800da4fd94050479a7cc29718cb0411e6
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2025-09-13 22:35:51 +0300

    Update crates demo to use Obj
    * Create crates only once
    * Add bbox-frustum intersection check

--- demos/src/bin/crates.rs
+++ demos/src/bin/crates.rs
@@ -4,8 +4,7 @@ use re::prelude::*;
 
 use re::math::color::gray;
 use re::render::{
-    Batch, Camera, ModelToProj,
-    cam::{FirstPerson, Fov},
+    ModelToProj, cam::FirstPerson, cam::Fov, clip::Status::Hidden, scene::Obj,
 };
 
 use re_front::sdl2::Window;
@@ -24,13 +23,11 @@ fn main() {
         |frag: Frag<Color3f>| frag.var.to_color4(),
     );
     let crate_shader = shader::new(
-        |v: Vertex3<_>, mvp: &Mat4x4<ModelToProj>| {
-            vertex(mvp.apply(&v.pos), v.attrib)
+        |v: Vertex3<Normal3>, mvp: &Mat4x4<ModelToProj>| {
+            let [x, y, z] = ((v.attrib + splat(1.0)) / 2.0).0;
+            vertex(mvp.apply(&v.pos), rgb(x, y, z))
         },
-        |frag: Frag<Normal3>| {
-            let [x, y, z] = ((frag.var + splat(1.0)) / 2.0).0;
-            rgb(x, y, z).to_color4()
-        },
+        |frag: Frag<Color3f>| frag.var.to_color4(),
     );
 
     let (w, h) = win.dims;
@@ -40,10 +37,12 @@ fn main() {
         .perspective(Fov::Diagonal(degs(90.0)), 0.1..1000.0);
 
     let floor = floor();
-    let krate = Cube { side_len: 2.0 }.build();
+    let crates = crates();
 
     win.run(|frame| {
+        //
         // Camera
+        //
 
         let mut cam_vel = Vec3::zero();
 
@@ -61,16 +60,18 @@ fn main() {
         }
 
         let ms = ep.relative_mouse_state();
-        let d_az = turns(ms.x() as f32) * -0.001;
-        let d_alt = turns(ms.y() as f32) * 0.001;
-
-        cam.transform.rotate(d_az, d_alt);
+        cam.transform.rotate(
+            turns(ms.x() as f32) * -0.001,
+            turns(ms.y() as f32) * 0.001,
+        );
         cam.transform
             .translate(cam_vel.mul(frame.dt.as_secs_f32()));
 
         let flip = scale3(1.0, -1.0, -1.0).to();
 
+        //
         // Render
+        //
 
         let world_to_project = flip.then(&cam.world_to_project());
 
@@ -78,40 +79,72 @@ fn main() {
             .viewport(cam.viewport)
             .context(&frame.ctx);
 
-        batch
-            .clone()
-            .mesh(&floor)
-            .uniform(&world_to_project)
-            .shader(floor_shader)
-            .target(&mut frame.buf)
-            .render();
-
-        let krate = batch.clone().mesh(&krate);
-
-        let n = 30;
-        for i in (-n..=n).step_by(5) {
-            for j in (-n..=n).step_by(5) {
-                let pos = translate3(i as f32, 0.0, j as f32).to();
-                krate
-                    // TODO Try to get rid of clone
+        // Floor
+        {
+            let Obj { geom, bbox, tf } = &floor;
+            let model_to_project = tf.then(&world_to_project);
+            if bbox.visibility(&model_to_project) != Hidden {
+                batch
                     .clone()
-                    .uniform(&pos.then(&world_to_project))
-                    // TODO Allow setting shader before uniform
-                    .shader(crate_shader)
-                    // TODO storing &mut target makes Batch not Clone, maybe
-                    //      pass to render() instead. OTOH then a Frame::batch
-                    //      helper wouldn't be as useful. Maybe just wrap the
-                    //      target in a RefCell?
+                    .mesh(&geom)
+                    .uniform(&model_to_project)
+                    .shader(floor_shader)
                     .target(&mut frame.buf)
                     .render();
             }
         }
+
+        // Crates
+
+        for Obj { geom, bbox, tf } in &crates {
+            frame.ctx.stats.borrow_mut().objs.i += 1;
+
+            let model_to_project = tf.then(&world_to_project);
+
+            // TODO Also if `Visible`, no further clipping or culling needed
+            if bbox.visibility(&model_to_project) == Hidden {
+                continue;
+            }
+
+            batch
+                // TODO Try to get rid of clone
+                .clone()
+                .mesh(&geom)
+                .uniform(&model_to_project)
+                // TODO Allow setting shader before uniform
+                .shader(crate_shader)
+                // TODO storing &mut target makes Batch not Clone, maybe
+                //      pass to render() instead. OTOH then a Frame::batch
+                //      helper wouldn't be as useful. Maybe just wrap the
+                //      target in a RefCell?
+                .target(&mut frame.buf)
+                .render();
+
+            frame.ctx.stats.borrow_mut().objs.o += 1;
+        }
+
         Continue(())
     })
     .expect("should run")
 }
 
-fn floor() -> Mesh<Color3f> {
+fn crates() -> Vec<Obj<Normal3>> {
+    let krate = Cube { side_len: 2.0 }.build();
+    let obj = Obj::new(krate);
+
+    let mut res = vec![];
+    let n = 30;
+    for i in (-n..=n).step_by(5) {
+        for j in (-n..=n).step_by(5) {
+            res.push(Obj {
+                tf: translate3(i as f32, 0.0, j as f32).to(),
+                ..obj.clone()
+            });
+        }
+    }
+    res
+}
+fn floor() -> Obj<Color3f> {
     let mut bld = Mesh::builder();
 
     let size = 50;
@@ -145,5 +178,5 @@ fn floor() -> Mesh<Color3f> {
             }
         }
     }
-    bld.build()
+    Obj::new(bld.build())
 }
