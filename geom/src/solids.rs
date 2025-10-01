52ee97371b13537f263b52712286c95a01fbd7ec
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2025-10-01 16:35:01 +0300

    Add Build trait to build meshes with different vertex attribs
    Impl Build for lathe-based objects.

--- geom/src/solids.rs
+++ geom/src/solids.rs
@@ -4,7 +4,17 @@
 mod lathe;
 mod platonic;
 
+use re::geom::{Mesh, mesh::Builder};
+
 #[cfg(feature = "std")]
 pub use lathe::*;
 
 pub use platonic::*;
+
+pub trait Build<A>: Sized {
+    fn build(self) -> Mesh<A>;
+
+    fn builder(self) -> Builder<A> {
+        self.build().into_builder()
+    }
+}
