b863dda1f73917a60b1d78a290d760b0c4ade13c
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2025-11-15 00:45:10 +0200

    Add sphere type and ray-sphere intersection test

--- geom/src/isect.rs
+++ geom/src/isect.rs
@@ -1,6 +1,6 @@
 use retrofire_core::{
-    geom::{Plane3, Ray},
-    math::{ApproxEq, Point3, Vec3},
+    geom::{Plane3, Ray, Ray3, Sphere},
+    math::{ApproxEq, Point3},
     render::scene::BBox,
 };
 
@@ -16,8 +16,10 @@ pub trait Intersect<T> {
     fn intersect(&self, other: &T) -> Self::Result;
 }
 
-impl<B> Intersect<Plane3<B>> for Ray<Point3<B>> {
-    type Result = Option<(f32, Point3<B>)>;
+type RayIntersect3<B> = Option<(f32, Point3<B>)>;
+
+impl<B> Intersect<Plane3<B>> for Ray3<B> {
+    type Result = RayIntersect3<B>;
 
     /// Returns the unique intersection point of `self` and a plane,
     /// or `None` if they do not intersect.
@@ -51,8 +53,8 @@ impl<B> Intersect<Plane3<B>> for Ray<Point3<B>> {
     }
 }
 
-impl<B: Default> Intersect<BBox<B>> for Ray<Point3<B>> {
-    type Result = Option<(f32, Point3<B>)>; // Only closest for now
+impl<B: Default> Intersect<BBox<B>> for Ray3<B> {
+    type Result = RayIntersect3<B>; // Only closest for now
 
     /// Returns the nearest intersection point of `self` and a box,
     /// or `None` if they do not intersect.
@@ -89,6 +91,7 @@ impl<B: Default> Intersect<BBox<B>> for Ray<Point3<B>> {
         let mut res = None;
         for plane in &planes {
             if let pt @ Some((t, p)) = self.intersect(plane)
+                // FIXME may be false due to rounding errors
                 && bbox.contains(&p)
                 && res.is_none_or(|(u, _)| t < u)
             {
@@ -99,9 +102,103 @@ impl<B: Default> Intersect<BBox<B>> for Ray<Point3<B>> {
     }
 }
 
+#[cfg(feature = "std")]
+impl<B> Intersect<Sphere<B>> for Ray3<B> {
+    type Result = RayIntersect3<B>; // Only closest for now
+
+    /// Returns the intersection point of `self` and a sphere closest to the
+    /// origin of `self`, or `None` if they do not intersect.
+    ///
+    /// # Examples
+    /// ```
+    /// ```
+    fn intersect(&self, &Sphere(center, r): &Sphere<B>) -> Self::Result {
+        let &Ray(orig, dir) = self;
+
+        //             > r, no intersection
+        // If |C - C'| = r, one    -"-
+        //             < r, two    -"-
+        //           _______
+        //          /   |r  \
+        //         |    C    |
+        //          \___|___/
+        //             _|
+        //   O--------+-C'----> d
+        //
+        //
+        // Nb. this is the same as the discriminant check later
+        //     Is this even a fast path?
+        let center_dir = center - orig;
+        let c_prime = center_dir.vector_project(&dir);
+        if (c_prime - center_dir).len_sqr() > r * r {
+            return None;
+        }
+
+        // Find point p = (x, y, z)
+        //
+        // Sphere equation:
+        //   (x-cx)^2 + (y-cy)^2 + (z-cz)^2 = r^2
+        // or in vector form
+        //   (p-c) · (p-c) = r^2
+        //
+        // Ray equation:
+        //   p = o + t·d
+        //
+        // Intersection:
+        //
+        //   Substitute ray equation to sphere equation:
+        //   (o + t·d - c) · (o + t·d - c) = r^2
+        //
+        //   Multiply out
+        //   o (o + td - c) + t·d (o + td - c) - c (o + td - c) = r^2
+        //
+        //   Distribute
+        //   o·o + o·td - o·c + o·td + td·td - td·c - c·o - c·td + c·c = r^2
+        //
+        //   Reorder
+        //   td·td + o·td + o·td - td·c + o·o - o·c - c·o + c·c - r^2 = 0
+        //
+        //   Factor out t's
+        //   (d·d) t^2 + (o·d + o·d - c·d) t + o·o - 2o·c + c·c - r^2 = 0
+        //
+        // Solve quadratic equation:
+        //   (d·d) t^2 + 2(o - c)·d t + (o - c)·(o - c) - r^2 = 0
+
+        let a = dir.len_sqr(); // >= 0
+        let b = -2.0 * center_dir.dot(&dir); // center_dir = c - o
+        let c = center_dir.len_sqr() - r * r;
+
+        if a.approx_eq(&0.0) {
+            // Only possible if dir == 0
+            return None;
+        }
+
+        let discr = b * b - 4.0 * a * c;
+        // Handled at the top
+        debug_assert!(discr >= 0.0, "this should not happen");
+
+        use retrofire_core::math::float::f32;
+        let sqrt = f32::sqrt(discr);
+        // sqrt >= 0.0, thus t0 <= t1 always
+        let (t0, t1) = (-b - sqrt, -b + sqrt);
+        let t = if t1 < 0.0 {
+            // sphere is behind ray
+            //dbg!(t0, t1, a, b, c, discr);
+            return None;
+        } else if t0 < 0.0 {
+            // ray origin is inside sphere
+            t1
+        } else {
+            t0.min(t1)
+        };
+        let t = t / (2.0 * a);
+        Some((t, orig + t * dir))
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use retrofire_core::math::{Linear, pt3, vec3};
+    use retrofire_core::math::{Linear, Vec3, pt3, vec3};
 
     use super::*;
 
@@ -262,6 +359,46 @@ mod tests {
             let empty = BBox::<()>(pt3(-1.0, -1.0, 1.0), pt3(1.0, 1.0, -1.0));
             let ray = Ray(pt3(0.0, 0.0, -2.0), vec3(0.0, 0.0, 1.0));
             assert_eq!(ray.intersect(&empty), None);
+        }
+    }
+
+    // TODO until sqrt has a fallback
+    #[cfg(feature = "std")]
+    mod ray_sphere {
+        use super::*;
+
+        const SPHERE: Sphere = Sphere(pt3(0.0, 0.0, 1.0), 2.0);
+
+        #[test]
+        fn ray_passes_through_sphere() {
+            let ray: Ray3 = Ray(pt3(0.0, 0.0, -3.0), vec3(0.0, 0.0, 2.0));
+            assert_eq!(
+                ray.intersect(&SPHERE),
+                Some((1.0, pt3(0.0, 0.0, -1.0)))
+            );
+        }
+        #[test]
+        fn ray_tangent_to_sphere() {
+            let ray: Ray3 = Ray(pt3(0.0, 2.0, -3.0), vec3(0.0, 0.0, 2.0));
+            assert_eq!(ray.intersect(&SPHERE), Some((2.0, pt3(0.0, 2.0, 1.0))));
+        }
+
+        #[test]
+        fn ray_origin_inside_sphere() {
+            let ray: Ray3 = Ray(pt3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 2.0));
+            assert_eq!(ray.intersect(&SPHERE), Some((1.5, pt3(0.0, 0.0, 3.0))));
+
+            let ray: Ray3 = Ray(pt3(0.0, 0.0, 2.0), vec3(0.0, 0.0, 2.0));
+            assert_eq!(ray.intersect(&SPHERE), Some((0.5, pt3(0.0, 0.0, 3.0))));
+        }
+
+        #[test]
+        fn no_intersection() {
+            let ray: Ray3 = Ray(pt3(0.0, 0.0, -3.0), vec3(0.0, 0.0, -1.0));
+            assert_eq!(ray.intersect(&SPHERE), None);
+
+            let ray: Ray3 = Ray(pt3(0.0, 0.0, -3.0), vec3(0.0, 1.0, 1.0));
+            assert_eq!(ray.intersect(&SPHERE), None);
         }
     }
 }
