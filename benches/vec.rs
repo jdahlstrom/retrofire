0a1a75e96d6fd64e597396ea21638e189422e3fc
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2026-05-12 14:20:05 +0300

    Benchmark vec angle variants

--- benches/vec.rs
+++ benches/vec.rs
@@ -1,11 +1,51 @@
 //! Triangle clipping benchmarks.
 
 use divan::Bencher;
-use divan::counter::ItemsCount;
-use retrofire_core::{
-    math::rand::{DefaultRng, Distrib},
-    math::{Vec3, splat},
+
+use retrofire_core::math::{
+    rand::{DefaultRng, Distrib},
+    vec::dot,
+    {Vec3, acos, splat},
 };
+
+#[divan::bench]
+fn angle(b: Bencher) {
+    let rng = &mut DefaultRng::default();
+    let vecs = splat(-1e6)..splat(1e6);
+
+    b.with_inputs(|| (vecs.sample(rng), vecs.sample(rng)))
+        .counter(1u32)
+        .bench_local_values(|(v, u): (Vec3, Vec3)| {
+            acos(dot(&v.0, &u.0) / (v.len() * u.len()))
+        });
+}
+
+#[divan::bench]
+fn angle_faster(b: Bencher) {
+    let rng = &mut DefaultRng::default();
+    let vecs = splat(-1e6)..splat(1e6);
+
+    b.with_inputs(|| (vecs.sample(rng), vecs.sample(rng)))
+        .counter(1u32)
+        .bench_local_values(|(v, u): (Vec3, Vec3)| {
+            acos(dot(&v.0, &u.0) / (dot(&v.0, &v.0) * dot(&v.0, &v.0)).sqrt())
+        });
+}
+
+#[divan::bench]
+fn angle_approx(b: Bencher) {
+    let rng = &mut DefaultRng::default();
+    let vecs = splat(-1e6)..splat(1e6);
+
+    b.with_inputs(|| (vecs.sample(rng), vecs.sample(rng)))
+        .counter(1u32)
+        .bench_local_values(|(v, u): (Vec3, Vec3)| {
+            //let dot_v = v.x() * v.x() + v.y() * v.y() + v.z() * v.z();
+            //let dot_u = u.x() * u.x() + u.y() * u.y() + u.z() * u.z();
+            //let inv_len = fast_recip_sqrt(dot_v) * fast_recip_sqrt(dot_u);
+            acos(dot(&v.normalize_approx().0, &u.normalize_approx().0))
+        });
+}
 
 #[divan::bench]
 fn normalize_exact(b: Bencher) {
@@ -13,7 +53,7 @@ fn normalize_exact(b: Bencher) {
     let vecs = splat(-1e6)..splat(1e6);
 
     b.with_inputs(|| vecs.sample(rng))
-        .input_counter(|_| ItemsCount::new(1u32))
+        .counter(1u32)
         .bench_local_values(|v: Vec3| v.normalize());
 }
 
@@ -23,7 +63,7 @@ fn normalize_approx(b: Bencher) {
     let vecs = splat(-1e6)..splat(1e6);
 
     b.with_inputs(|| vecs.sample(rng))
-        .input_counter(|_| ItemsCount::new(1u32))
+        .counter(1u32)
         .bench_local_values(|v: Vec3| v.normalize_approx());
 }
 
