3ae52255ff7fdbfdbc6c21068029c3fa1c6a3f21
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2026-05-13 18:11:58 +0300

    Simplify bench counters

--- benches/vec.rs
+++ benches/vec.rs
@@ -1,7 +1,7 @@
 //! Triangle clipping benchmarks.
 
 use divan::Bencher;
-use divan::counter::ItemsCount;
+
 use retrofire_core::{
     math::rand::{DefaultRng, Distrib},
     math::{Vec3, splat},
@@ -13,7 +13,7 @@ fn normalize_exact(b: Bencher) {
     let vecs = splat(-1e6)..splat(1e6);
 
     b.with_inputs(|| vecs.sample(rng))
-        .input_counter(|_| ItemsCount::new(1u32))
+        .counter(1u32)
         .bench_local_values(|v: Vec3| v.normalize());
 }
 
@@ -23,7 +23,7 @@ fn normalize_approx(b: Bencher) {
     let vecs = splat(-1e6)..splat(1e6);
 
     b.with_inputs(|| vecs.sample(rng))
-        .input_counter(|_| ItemsCount::new(1u32))
+        .counter(1u32)
         .bench_local_values(|v: Vec3| v.normalize_approx());
 }
 
