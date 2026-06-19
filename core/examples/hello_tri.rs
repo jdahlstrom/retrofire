cb9fd6abc716ea80d2ca37f93c6fddd41e53729d
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2026-06-19 19:29:07 +0300

    Fixup hello_tri

--- core/examples/hello_tri.rs
+++ core/examples/hello_tri.rs
@@ -4,8 +4,8 @@ use retrofire_core::util::dims;
 
 fn main() {
     let verts = [
-        vertex(pt3(-1.0, 1.0, 0.0), rgb(1.0, 0.0, 0.0)),
-        vertex(pt3(1.0, 1.0, 0.0), rgb(0.0, 0.8, 0.0)),
+        vertex(pt3(1.0, 1.0, 0.0), rgb(1.0, 0.2, 0.0)),
+        vertex(pt3(-1.0, 1.0, 0.0), rgb(0.0, 0.8, 0.2)),
         vertex(pt3(0.0, -1.0, 0.0), rgb(0.4, 0.4, 1.0)),
     ];
 
@@ -28,7 +28,7 @@ fn main() {
         |frag: Frag<Color3f<_>>, _| frag.var.to_color4(),
     );
 
-    let dims @ Dims(w, h) = dims::VGA_640_480;
+    let dims = dims::VGA_640_480;
     let modelview = translate((0.0, 0.0, 2.0)).to();
     let project = perspective(1.0, dims.aspect(), 0.1..1000.0);
     let viewport = viewport(dims.into());
@@ -44,17 +44,16 @@ fn main() {
         &mut framebuf,
         &Context::default(),
     );
-
-    let center_pixel = framebuf[[w / 2, h / 2]];
-
-    if cfg!(feature = "fp") {
-        assert_eq!(center_pixel, rgba(151, 128, 187, 255));
-    } else {
-        assert_eq!(center_pixel, rgba(114, 102, 128, 255));
-    }
     #[cfg(feature = "std")]
     {
         use retrofire_core::util::pnm;
-        pnm::save_ppm("triangle.ppm", framebuf).unwrap();
+        pnm::save_ppm("triangle.ppm", &framebuf).unwrap();
+    }
+
+    let center_pixel = framebuf[[dims.0 / 2, dims.1 / 2]];
+    if cfg!(feature = "fp") {
+        assert_eq!(center_pixel, rgba(152, 130, 187, 255));
+    } else {
+        assert_eq!(center_pixel, rgba(115, 114, 140, 255));
     }
 }
