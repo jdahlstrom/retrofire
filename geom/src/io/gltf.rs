e51e45a3670531884aeb35e183ad40530c90fbe8
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2026-06-24 23:56:04 +0300

    Better error mgt

--- geom/src/io/gltf.rs
+++ geom/src/io/gltf.rs
@@ -8,12 +8,17 @@ use retrofire_core::geom::{Mesh, Normal3};
 #[derive(Debug)]
 pub enum Error {
     Gltf(gltf::Error),
+    NoMeshesFound,
+    NoPositionsFound,
+    NoNormalsFound,
+    NoIndicesFound,
 }
+use Error::*;
 
 pub fn load_gltf(path: impl AsRef<Path>) -> Result<Mesh<Normal3>, Error> {
-    let (doc, bufs, _imgs) = import(path).map_err(Error::Gltf)?;
+    let (doc, bufs, _imgs) = import(path).map_err(Gltf)?;
 
-    let mesh = doc.meshes().next().expect("at least one mesh");
+    let mesh = doc.meshes().next().ok_or(NoMeshesFound)?;
 
     let mut res = Mesh::builder();
 
@@ -23,13 +28,16 @@ pub fn load_gltf(path: impl AsRef<Path>) -> Result<Mesh<Normal3>, Error> {
         }
         let reader = prim.reader(|a| Some(&bufs[a.index()]));
 
-        let positions = reader.read_positions().unwrap();
-        let normals = reader.read_normals().unwrap();
+        let positions = reader.read_positions().ok_or(NoPositionsFound)?;
+        let normals = reader.read_normals().ok_or(NoNormalsFound)?;
 
         for (pos, norm) in positions.zip(normals) {
             res.push_vert(pos.into(), norm.into());
         }
-        let mut indices = reader.read_indices().unwrap().into_u32();
+        let mut indices = reader
+            .read_indices()
+            .ok_or(NoIndicesFound)?
+            .into_u32();
 
         for _ in 0..indices.len() / 3 {
             let i = indices.next().unwrap();
