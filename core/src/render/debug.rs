4c5671938cad2f44488c17d7fab6d10f548895f2
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2026-07-09 02:31:09 +0300

    Avoid allocating each debug normal separately

--- core/src/render/debug.rs
+++ core/src/render/debug.rs
@@ -56,28 +56,10 @@ pub fn dir_to_rgb<B>(v: Vec3<B>) -> Color4f {
 }
 
 /// Draws an illustration of a ray.
-pub fn ray<B>(o: Point3<B>, dir: Vec3<B>) -> DbgBatch<B> {
-    let mut b = dir.cross(&Vec3::Y);
-    if b.len_sqr() < 1e-6 {
-        b = dir.cross(&Vec3::X);
-    }
-    let b = b.normalize_or_zero();
-    let c = dir.cross(&b).normalize_or_zero();
-
-    let (head_w, head_h) = (0.02, 0.04);
-    let a = o + dir - head_h * dir.normalize_or_zero();
-    let b = head_w * b;
-    let c = head_w * c;
-
-    let verts = [o, o + dir, a + b, a - b, a + c, a - c]
-        .map(|p| vertex(p, dir_to_rgb(dir)));
-    #[rustfmt::skip]
-    let edges = [
-        [0, 1], [1, 2], [1, 3], [1, 4], [1, 5],
-        [2, 4], [2, 5], [3, 4], [3, 5],
-    ].map(Edge::from);
-
-    DbgBatch::new(edges.to_vec(), verts.to_vec())
+pub fn ray<B>(orig: Point3<B>, dir: Vec3<B>) -> DbgBatch<B> {
+    let mut batch = DbgBatch::new();
+    batch.ray(orig, dir);
+    batch
 }
 
 /// Draws a unit-length ray denoting the normal vector of a vertex.
@@ -127,7 +109,7 @@ pub fn cuboid<B>(v0: Point3<B>, v1: Point3<B>) -> DbgBatch<B> {
         [0, 4], [1, 5], [2, 6], [3, 7],
     ].map(Edge::from);
 
-    DbgBatch::new(edges.to_vec(), verts.to_vec())
+    DbgBatch::with(edges.to_vec(), verts.to_vec())
 }
 
 /// Draws the smallest axis-aligned box that contains a set of vertices.
@@ -137,7 +119,7 @@ pub fn bbox<B>(pts: &[impl Pos<Type = Point3<B>>]) -> DbgBatch<B> {
 }
 
 /// Creates a `DbgMesh` object from a mesh.
-pub fn mesh<A: Clone, B>(mesh: &Mesh<A, B>) -> DbgMesh<A, B> {
+pub fn mesh<A: Clone, B>(mesh: &Mesh<A, B>) -> DbgMesh<'_, A, B> {
     DbgMesh(mesh, DbgBatch::default())
 }
 
@@ -149,7 +131,20 @@ impl<'a, A: Clone, B> DbgMesh<'a, A, B> {
     /// Enables drawing the edges as a wireframe representation.
     #[must_use]
     pub fn edges(mut self) -> Self {
-        self.1.append(wireframe(self.0));
+        let Mesh { faces, verts } = self.0;
+
+        let n_verts = self.1.verts.len();
+        let edges = faces
+            .iter()
+            .flat_map(Tri::edges)
+            .map(|Edge(a, b)| Edge(a + n_verts, b + n_verts));
+        let verts = verts
+            .iter()
+            .map(|v| vertex(v.pos, dir_to_rgb(v.pos.to_vec())));
+
+        self.1.prims.extend(edges);
+        self.1.verts.extend(verts);
+
         self
     }
 
@@ -157,9 +152,7 @@ impl<'a, A: Clone, B> DbgMesh<'a, A, B> {
     #[must_use]
     pub fn face_normals(mut self, scale: f32) -> Self {
         for tri in self.0.faces() {
-            // TODO inefficient
-            self.1
-                .append(face_normal(&tri.map(|v| v.clone()), scale));
+            self.1.face_normal(&tri.map(|v| v.clone()), scale);
         }
         self
     }
@@ -187,10 +180,9 @@ impl<'a, A: Clone, B> DbgMesh<'a, A, B> {
 
 impl<'a, B> DbgMesh<'a, Normal3, B> {
     #[must_use]
-    pub fn normals(mut self, scale: f32) -> Self {
+    pub fn vertex_normals(mut self, scale: f32) -> Self {
         for v in &self.0.verts {
-            // TODO inefficient
-            self.1.append(vertex_normal(v, scale));
+            self.1.vertex_normal(v, scale);
         }
         self
     }
@@ -204,21 +196,8 @@ impl<'a, B> DbgMesh<'a, Normal3, B> {
 /// debug::mesh(mesh).edges().patch()
 /// ```
 #[must_use]
-pub fn wireframe<A: Clone, B>(mesh: &Mesh<A, B>) -> DbgBatch<B> {
-    let edges: Vec<_> = mesh
-        .faces
-        .iter()
-        .copied()
-        .flat_map(|Tri([i, j, k])| [Edge(i, j), Edge(j, k), Edge(k, i)])
-        .collect();
-
-    DbgBatch::new(
-        edges,
-        mesh.verts
-            .iter()
-            .map(|v| vertex(v.pos, dir_to_rgb(v.pos.to_vec())))
-            .collect::<Vec<_>>(),
-    )
+pub fn wireframe<A: Clone, B>(msh: &Mesh<A, B>) -> DbgBatch<B> {
+    mesh(msh).edges().batch()
 }
 
 /// Draws a circle on the XY plane with the given center and radius.
@@ -236,7 +215,7 @@ pub fn circle<B>(o: Point3<B>, r: f32) -> DbgBatch<B> {
 
     let edges: Vec<_> = (0..RES).map(|i| Edge(i, i + 1)).collect();
 
-    DbgBatch::new(edges, verts)
+    DbgBatch::with(edges, verts)
 }
 
 /// Draws a wireframe sphere with the given center and radius.
@@ -266,15 +245,63 @@ pub fn sphere<B>(o: Point3<B>, r: f32) -> DbgBatch<B> {
         })
         .collect();
 
-    DbgBatch::new(edges, verts)
+    DbgBatch::with(edges, verts)
 }
 
 impl<B> DbgBatch<B> {
-    fn new(prims: Vec<Edge<usize>>, verts: Vec<Vertex3<Color4f, B>>) -> Self {
+    fn new() -> Self {
+        DbgBatch::<B>::default()
+    }
+
+    fn with(prims: Vec<Edge<usize>>, verts: Vec<Vertex3<Color4f, B>>) -> Self {
         DbgBatch::<B>::default()
             .primitives(prims)
             .vertices(verts)
-            .shader(Shader)
+    }
+
+    fn ray(&mut self, orig: Point3<B>, dir: Vec3<B>) -> &mut Self {
+        let mut b = dir.cross(&Vec3::Y);
+        if b.len_sqr() < 1e-6 {
+            b = dir.cross(&Vec3::X);
+        }
+        let b = b.normalize_or_zero();
+        let c = dir.cross(&b).normalize_or_zero();
+
+        let (head_w, head_h) = (0.02, 0.04);
+        let a = orig + dir - head_h * dir.normalize_or_zero();
+        let b = head_w * b;
+        let c = head_w * c;
+
+        let verts = [orig, orig + dir, a + b, a - b, a + c, a - c]
+            .map(|p| vertex(p, dir_to_rgb(dir)));
+        #[rustfmt::skip]
+        let edges = [
+            [0, 1], [1, 2], [1, 3], [1, 4], [1, 5],
+            [2, 4], [2, 5], [3, 4], [3, 5],
+        ].map(Edge::from);
+
+        let n = self.verts.len();
+        self.verts.extend(verts);
+        self.prims
+            .extend(edges.into_iter().map(|e| Edge(e.0 + n, e.1 + n)));
+
+        self
+    }
+
+    fn vertex_normal(
+        &mut self,
+        v: &Vertex3<Normal3, B>,
+        scale: f32,
+    ) -> &mut Self {
+        self.ray(v.pos, scale * v.attrib.to())
+    }
+
+    fn face_normal<A>(
+        &mut self,
+        tri: &Tri<Vertex3<A, B>>,
+        scale: f32,
+    ) -> &mut Self {
+        self.ray(tri.centroid(), scale * tri.normal().to())
     }
 }
 
